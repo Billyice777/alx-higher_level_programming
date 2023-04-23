9826026e92f5d0ad9f0f99cc8ad0e7793c938188
Billyice777 <uwemedimoekpewohp@gmail.com>
2023-04-23 21:22:54 +0000

    Task 11

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a Geometry Class"""
+
+
+class BaseGeometry:
+    """A class that represents a Geometric object"""
+
+    def area(self):
+        """Definition of area method.
+           Raises an Exception with message 'area() is not implemented'"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value to be an int grater than 0.
+        Args:
+            name (str): name of the value
+            value (unknown): to be validated.
+        Raises:
+            TypeError: if value is not an int.
+            ValueError: if value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
